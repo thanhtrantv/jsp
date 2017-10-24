59555a02484618ef440309e557a923b547938440
helpme.tvt@gmail.com <tran.than@heb.com>
2017-10-24 17:14:20 +0700

    first commit

---
+++ out/artifacts/hello2_war_exploded/index.jsp
@@ -0,0 +1,10 @@
+<%@ page import="java.time.LocalDate" %>
+<%@ page import="java.time.LocalDateTime" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+    <head><title>Hello word!!</title></head>
+    <body>
+        Hello World!<br/>
+        <%="Time is " + LocalDateTime.now()%>
+    </body>
+</html>
---
+++ src/com/helloword/HelloServlet.java
@@ -0,0 +1,16 @@
+package com.helloword;
+
+import java.io.IOException;
+
+/**
+ * Created by thanhtran on 10/24/2017.
+ */
+public class HelloServlet extends javax.servlet.http.HttpServlet {
+	protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+		System.out.println("post request");
+	}
+
+	protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+		System.out.println("get request");
+	}
+}
